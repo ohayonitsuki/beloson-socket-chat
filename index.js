7be920e88a5a3d402aea0e9f583d5c537907c32f
ohayonitsuki <nk.mihairofu@gmail.com>
2025-05-23 20:49:10 +0300

    Реорганизовал обработчик маршрутов чтобы использовать sendFile вместо него, добавил скрипт html

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const app = express();
+const server = createServer(app);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
