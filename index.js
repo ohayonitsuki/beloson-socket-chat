bb8b8aab215ffef28bdaa169dfd127b7ce299d99
ohayonitsuki <nk.mihairofu@gmail.com>
2025-05-24 15:14:44 +0300

    Теперь при отправке сообщения пользователем, мы получаем это сообщение в логах

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,9 +14,14 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+io.on('connection', (socket) => {
+	io.on('connection', (socket) => {
+		socket.on('chat message', (msg) => {
+			console.log('message: ' + msg);
+		});
+	});
 });
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
